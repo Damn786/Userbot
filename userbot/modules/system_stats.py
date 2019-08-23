046d7f92c8cd4154248ee771c1ab54d3c3302fff
Damn786 <54433976+Damn786@users.noreply.github.com>
2019-08-23 16:51:01 +0530

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -116,9 +116,9 @@ async def amireallyalive(alive):
     if not alive.text[0].isalpha() and alive.text[0] not in ("/", "#", "@", "!"):
         await alive.edit(
             "`"
-           "Work For a Clause Not For Applause,
-            Live Life to Express Not to Impress @Kingwillbealwaysking\n\n"
-            \n"
+           "Work For a Clause Not For Applause \n\n"
+            f" Live Life to Express Not to Impress @Kingwillbealwaysking\n"
+           
             "`"
             )
 
